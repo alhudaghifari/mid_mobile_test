2429b621ee096e660b981a75422bdb11f0d4dfc3
alhudaghifari <alhudaghifari@gmail.com>
2019-10-11 14:52:42 +0700

    filter activity and some drawable added

---
+++ app/src/main/java/id/member/test/view/activity/FilterActivity.kt
@@ -0,0 +1,12 @@
+package id.member.test.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+    }
+}
