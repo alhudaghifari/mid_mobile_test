1aef8cc162a304c1af4dc6ac5e49b9cc9bb6d928
alhudaghifari <alhudaghifari@gmail.com>
2019-10-11 07:55:09 +0700

    main activity done

--- app/src/main/java/id/member/test/view/MainActivity.kt
+++ app/src/main/java/id/member/test/view/MainActivity.kt
@@ -1,12 +1,79 @@
-package id.member.test
+package id.member.test.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import id.logique.hinoconnect.utils.SharedPrefManager
+import id.member.test.R
+import id.member.test.model.DrawerItem
+import id.member.test.view.adapter.CustomDrawerAdapter
+import id.member.test.view.fragment.AwardsFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_layout.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit  var adapter: CustomDrawerAdapter
+    private lateinit var dataList: MutableList<DrawerItem>
+    private lateinit var sharedPrefManager: SharedPrefManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPrefManager = SharedPrefManager(this)
+        dataList = mutableListOf()
+
+        dataList.add(DrawerItem("Home"))
+        dataList.add(DrawerItem("Cards"))
+        dataList.add(DrawerItem("Profile"))
+        dataList.add(DrawerItem("Logout"))
+
+        adapter = CustomDrawerAdapter(this, R.layout.item_drawer, dataList)
+
+        left_drawer.adapter = adapter
+        left_drawer.setOnItemClickListener(DrawerItemClickListener())
+
+        ivOpenMenuDrawer.setOnClickListener({
+            drawer_layout.openDrawer(GravityCompat.START)
+        })
+    }
+
+    fun selectItem(position: Int) {
+        when (position) {
+            0 -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.content_frame, AwardsFragment(), resources.getString(R.string.title_home))
+                    .commit()
+            }
+            3 -> {
+                sharedPrefManager.setLogin(false)
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else -> {
+            }
+        }
+        if (position != 1 && position != 2) {
+            adapter.setSelectedItem(position)
+            left_drawer.setItemChecked(position, true)
+            title = dataList[position].getItemName()
+        }
+        drawer_layout.closeDrawers()
+
+    }
+
+    private inner class DrawerItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(
+            parent: AdapterView<*>, view: View, position: Int,
+            id: Long
+        ) {
+            selectItem(position)
+        }
     }
 }
