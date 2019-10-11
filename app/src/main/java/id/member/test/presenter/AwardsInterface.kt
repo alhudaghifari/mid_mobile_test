2f61c8c843b854bfb12632d57407721034d77c97
alhudaghifari <alhudaghifari@gmail.com>
2019-10-11 14:53:18 +0700

    interface and presenter awards modified

--- app/src/main/java/id/member/test/presenter/AwardsInterface.kt
+++ app/src/main/java/id/member/test/presenter/AwardsInterface.kt
@@ -1,9 +1,15 @@
 package id.member.test.presenter
 
+import id.member.test.model.AwardsModel
+
 
 /**
  * Created by Alhudaghifari on 9:14 11/10/19
  *
  */
 interface AwardsInterface {
+    fun showLoading()
+    fun hideLoading()
+    fun callFinished(model: ArrayList<AwardsModel>)
+    fun showError(msg: String)
 }
--- app/src/main/java/id/member/test/presenter/AwardsPresenter.kt
+++ app/src/main/java/id/member/test/presenter/AwardsPresenter.kt
@@ -1,9 +1,17 @@
 package id.member.test.presenter
 
+import id.logique.hinoconnect.utils.Constant
+
 
 /**
  * Created by Alhudaghifari on 9:15 11/10/19
  *
  */
-class AwardsPresenter {
+class AwardsPresenter(private val view: AwardsInterface) {
+
+    fun callData() {
+        view.showLoading()
+        view.callFinished(Constant.getData())
+        view.hideLoading()
+    }
 }
