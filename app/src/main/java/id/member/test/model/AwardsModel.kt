a77d3e9fc311e26a1c870b5fb840633e2d0b41dd
alhudaghifari <alhudaghifari@gmail.com>
2019-10-11 08:03:32 +0700

    awards model added and modified

---
+++ app/src/main/java/id/member/test/model/AwardsModel.kt
@@ -0,0 +1,14 @@
+package id.member.test.model
+
+
+/**
+ * Created by Alhudaghifari on 8:01 11/10/19
+ *
+ */
+data class AwardsModel(
+    val id: Int,
+    val type: Int,
+    val point: Long,
+    val name: String,
+    val image: String
+)
