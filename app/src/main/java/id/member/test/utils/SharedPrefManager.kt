4349d115f434daa7e92ae36632463453b19d96a4
alhudaghifari <alhudaghifari@gmail.com>
2019-10-11 14:53:45 +0700

    shared pref modified

--- app/src/main/java/id/member/test/utils/SharedPrefManager.kt
+++ app/src/main/java/id/member/test/utils/SharedPrefManager.kt
@@ -15,6 +15,9 @@ class SharedPrefManager(context: Context) {
     private var pref: SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
 
     private val KEY_IS_LOGGED_IN = "isLoggedIn"
+    private val KEY_FILTER_POINT_VAL = "point"
+    private val KEY_FILTER_PRODUCT = "filterprod"
+    private val KEY_FILTER_VOUCHER = "voucherfilter"
 
     fun setLogin(isLoggedIn: Boolean) {
         editor = pref.edit()
@@ -26,4 +29,33 @@ class SharedPrefManager(context: Context) {
         return pref.getBoolean(KEY_IS_LOGGED_IN, false)
     }
 
+    fun setFilterPoint(point: Int) {
+        editor = pref.edit()
+        editor.putInt(KEY_FILTER_POINT_VAL, point)
+        editor.apply()
+    }
+
+    fun getFilterPoint(): Int {
+        return pref.getInt(KEY_FILTER_POINT_VAL, Constant.POINT_TYPE.POINT_MIDDLE.type)
+    }
+
+    fun setFilterProduct(filter: Boolean) {
+        editor = pref.edit()
+        editor.putBoolean(KEY_FILTER_PRODUCT, filter)
+        editor.apply()
+    }
+
+    fun getFilterProduct(): Boolean {
+        return pref.getBoolean(KEY_FILTER_PRODUCT, true)
+    }
+
+    fun setFilterVoucher(filter: Boolean) {
+        editor = pref.edit()
+        editor.putBoolean(KEY_FILTER_VOUCHER, filter)
+        editor.apply()
+    }
+
+    fun getFilterVoucher(): Boolean {
+        return pref.getBoolean(KEY_FILTER_VOUCHER, true)
+    }
 }
